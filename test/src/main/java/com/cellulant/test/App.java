0f7eb27d72da44924f8b2b8f050f86a0f6cd800e
drewheathens <mudavadie@gmail.com>
2019-08-22 14:47:50 +0300

    Added file

---
+++ test/src/main/java/com/cellulant/test/App.java
@@ -0,0 +1,16 @@
+package com.cellulant.test;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World kkkkk!");
+    }
+}
